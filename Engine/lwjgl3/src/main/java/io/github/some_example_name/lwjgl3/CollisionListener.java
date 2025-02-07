9be7df75a4a721b674016c0107b9aca6465ecaaf
weiloongc <2402219@sit.singaporetech.edu.sg>
2025-02-07 16:34:03 +0800

    Fixed File

--- Engine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/CollisionListener.java
+++ Engine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/CollisionListener.java
@@ -1,3 +1,5 @@
+package io.github.some_example_name.lwjgl3;
+
 public interface CollisionListener {
     void onCollisionEnter(Object objA, Object objB);
     void onCollisionStay(Object objA, Object objB);
--- Engine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/EntityManager.java
+++ Engine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/EntityManager.java
@@ -1,3 +1,4 @@
+package io.github.some_example_name.lwjgl3;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.util.ArrayList;
 import java.util.List;
