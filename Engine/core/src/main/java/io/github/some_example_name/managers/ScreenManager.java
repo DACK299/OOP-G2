2145f4619c069a59720150cab9ff486a0d7a7d58
BRIAN LEE ZHEXIAN <Brianlee6588@yahoo.com.sg>
2025-02-18 03:02:17 +0800

    Update ScreenManager.java
    Added main menu screen -BRIAN 18/2/2025
            screens.put("PLAY", new PlayScreen(game));
            screens.put("MENU", new MenuScreen(game));

--- Engine/core/src/main/java/io/github/some_example_name/managers/ScreenManager.java
+++ Engine/core/src/main/java/io/github/some_example_name/managers/ScreenManager.java
@@ -2,6 +2,7 @@ package io.github.some_example_name.managers;
 
 import com.badlogic.gdx.Screen;
 import io.github.some_example_name.Main;
+import io.github.some_example_name.screens.MenuScreen;
 import io.github.some_example_name.screens.PlayScreen;
 import java.util.HashMap;
 
@@ -10,25 +11,26 @@ public class ScreenManager {
     private Main game;
     private HashMap<String, Screen> screens;
     private Screen currentScreen;
-    
+
     private ScreenManager() {
         screens = new HashMap<>();
     }
-    
+
     public static ScreenManager getInstance() {
         if (instance == null) {
             instance = new ScreenManager();
         }
         return instance;
     }
-    
+
     public void initialize(Main game) {
         this.game = game;
         // Initialize screens
         screens.put("PLAY", new PlayScreen(game));
+        screens.put("MENU", new MenuScreen(game));
         // Add more screens here as needed
     }
-    
+
     public void showScreen(String screenName) {
         Screen screen = screens.get(screenName);
         if (screen != null) {
@@ -36,16 +38,16 @@ public class ScreenManager {
             if (currentScreen != null) {
                 currentScreen.dispose();
             }
-            
+
             currentScreen = screen;
             game.setScreen(screen);
         }
     }
-    
+
     public Screen getCurrentScreen() {
         return currentScreen;
     }
-    
+
     public void dispose() {
         for (Screen screen : screens.values()) {
             screen.dispose();
