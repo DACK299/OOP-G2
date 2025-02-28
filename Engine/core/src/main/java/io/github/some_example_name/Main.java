3a4f990552671c47c92061bdd3dc849e9f3980f5
DACK299 <darylangck@gmail.com>
2025-02-28 16:11:29 +0800

    added runningg

--- Engine/core/src/main/java/io/github/some_example_name/Main.java
+++ Engine/core/src/main/java/io/github/some_example_name/Main.java
@@ -6,10 +6,11 @@ import io.github.some_example_name.managers.ScreenManager;
 public class Main extends Game {
     @Override
     public void create() {
+        System.out.println("Running...");
         ScreenManager.getInstance().initialize(this, null, null);
         ScreenManager.getInstance().showScreen("MENU");
     }
-    
+
     @Override
     public void dispose() {
         super.dispose();
